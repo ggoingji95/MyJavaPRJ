692626ff7698ed40657b6d72cf0715d9c0bd78f8
ggoing95 <river1825@naver.com>
2024-03-11 15:43:20 +0900

    n번째 파일테스트 제발 되라 제발제발

--- src/main/java/Hello3.java
+++ src/main/java/Hello3.java
@@ -1,7 +1,7 @@
 public class Hello3 {
+    public static void main(String[]args){
 
-    public static void main(String[] args){
-    System.out.println("세번째 생성한 파일3");
+        System.out.println("n번째 테스트");
     }
 
 }
