44d37cbb6ca9720f6a152b3baa8c69eb0382a5cb
ggoing95 <river1825@naver.com>
2024-03-11 19:42:33 +0900

    My favorite travel

---
+++ src/main/java/Travel.java
@@ -0,0 +1,7 @@
+public class Travel {
+    public static void main(String[]args){
+        System.out.println("I want to go to newzealand"){
+        System.out.println("Edit i want to go to mongolia");
+
+    }
+}
