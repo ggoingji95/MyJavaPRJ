45420aba3ae8ec32dd4a248cee128981fa87d2e7
ggoing95 <river1825@naver.com>
2024-03-15 12:47:57 +0900

    am

---
+++ src/main/java/haye.java
@@ -0,0 +1,6 @@
+public class haye {
+    public.static.void main(String[]args){
+        System.out.println("what are you doing now?");
+        System.out.println("Edit i want go to my home");
+    }
+}
