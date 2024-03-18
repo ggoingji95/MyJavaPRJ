2d2e730d0218a9eac3ddbf85a6a3ea44de9ec046
ggoing95 <river1825@naver.com>
2024-03-18 15:06:54 +0900

    test

---
+++ src/main/java/Class02.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class code02_10{
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+
+        int num;
+
+        num = s.nextlnt();
+        System.out.println("사용자가 입력한 값 ==> " + num);
+
+        s.close();
+
+    }
+}
