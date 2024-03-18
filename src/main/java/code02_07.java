49e74beb785ac2aca570de935f742c394954d5ca
ggoing95 <river1825@naver.com>
2024-03-18 18:02:01 +0900

    code02_07

---
+++ src/main/java/code02_07.java
@@ -0,0 +1,20 @@
+public class code02_07 {
+    public static void main(String[]args){
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 50;
+
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result);
+
+        result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result);
+
+        result = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + result);
+
+        result = num1 / num2;
+        System.out.println(num1 + " / " + num2 + " = " + result);
+    }
+}
