bd38dc97db32fa9c19daf6ab96a8eb41939d3cfa
ggoing95 <river1825@naver.com>
2024-03-18 17:50:53 +0900

    code02_06

---
+++ src/main/java/code02_06.java
@@ -0,0 +1,11 @@
+public class code02_06 {
+    public static void main(String[]args){
+        int num1, num2, result;
+
+        num1 = 100;
+        num2 = 50;
+
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result);
+    }
+}
