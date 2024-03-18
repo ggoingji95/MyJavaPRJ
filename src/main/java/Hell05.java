9cba3ae30d021e18a2af1f25918e658362ed3a16
ggoing95 <river1825@naver.com>
2024-03-18 12:41:31 +0900

    king woosoek

--- src/main/java/Hell05.java
+++
@@ -1,6 +0,0 @@
-public class Hell05 {
-    public.static.void main(String[]args){
-        System.out.println("I am hungry");
-
-    }
-}
--- src/main/java/River.java
+++
@@ -1,5 +0,0 @@
-public class River {
-    public static void main(String[] args){
-        System.out.println("I want to go to river");
-    }
-}
