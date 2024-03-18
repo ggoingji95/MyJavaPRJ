ebd03c0bf1dd83efd8e6fd5071236297c07c3242
ggoing95 <river1825@naver.com>
2024-03-18 12:44:07 +0900

    hihi

---
+++ src/main/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public.static.void.mian(String[]args){
+        System.out.println("Java test mode");
+    }
+
+}
---
+++ src/main/java/hi.java
@@ -0,0 +1,5 @@
+public class hi {
+    public.static.void.mian(String[]ars){
+        System.out.println("life is very easy");
+    }
+}
