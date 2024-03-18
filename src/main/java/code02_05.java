1c48ff90a06a86c91f908dafa44a43c6c08a60b7
ggoing95 <river1825@naver.com>
2024-03-18 17:51:11 +0900

    code02_05

---
+++ src/main/java/code02_05.java
@@ -0,0 +1,11 @@
+public class code02_05 {
+    public static void main(String[]args){
+
+        int a = 100;
+        int b = 200;
+
+        int c = 100 + 200;
+        System.out.println(a + " + " + b + " = " + c );
+
+    }
+}
