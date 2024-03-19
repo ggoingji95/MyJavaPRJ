9ecee76f3fc9e567bb1f15a1ad2000c475c21401
ggoing95 <river1825@naver.com>
2024-03-19 11:29:32 +0900

    lab02_071

---
+++ src/main/java/code02_071.java
@@ -0,0 +1,25 @@
+public class code02_071 {
+    public static void main(String[]args){
+
+        int num1, num2, result;
+
+        num1 = 300;
+        num2 = 25;
+
+        result = num1 + num2;
+            System.out.println(num1 + " + " + num2 + " = " + result);
+
+            result = num1 - num2;
+            System.out.println(num1 + " - " + num2 + " = " + result);
+
+            result = num1 * num2;
+            System.out.println(num1 + " * " + num2 + " = " + result);
+
+            result = num1 / num2;
+            System.out.println(num1 + " / " + num2 + " = " + result);
+
+
+
+
+    }
+}
