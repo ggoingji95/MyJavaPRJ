1b6155c64a6f00e7d05cbfc35d91c179949c532f
ggoingji95 <river1825@naver.com>
2024-03-21 08:53:08 +0900

    code03_022

---
+++ src/main/java/code03_022.java
@@ -0,0 +1,27 @@
+ public class code03_022 {
+        public static void main(String[] args){
+
+            int n1, n2, res;
+
+            n1 = 18320;
+            n2 = 15500;
+
+            res = n1 + n2;
+            System.out.println(res);
+
+            res = n1 - n2;
+            System.out.println(res);
+
+            res = n1 * n2;
+            System.out.println(res);
+
+            res = n1 / n2;
+            System.out.println(res);
+
+            res = n1 % n2;
+            System.out.println(res);
+
+        }
+    }
+
+
