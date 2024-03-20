902f8bffa2ea13a7bb2735c97c16f0b309c7a1d5
ggoingji95 <river1825@naver.com>
2024-03-20 19:55:01 +0900

    code05__.java

--- src/main/java/Travel.java
+++
@@ -1,7 +0,0 @@
-public class Travel {
-    public static void main(String[]args){
-        System.out.println("I want to go to newzealand"){
-        System.out.println("Edit i want to go to mongolia");
-
-    }
-}
