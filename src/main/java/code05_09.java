9c896cbad24700192fab97bd142200fc7bd32933
ggoingji95 <river1825@naver.com>
2024-03-22 16:38:10 +0900

    code05_03

---
+++ src/main/java/code05_09.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class code05_09 {
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int select;
+
+        System.out.print("1~3 중에 선택하세요 : ");
+        select = s.nextInt();
+
+        switch(select) {
+            case 1:
+                System.out.println("1을 선택했습니다.");
+                break;
+
+            case 2:
+                System.out.println("2를 선택했습니다.");
+                break;
+
+            case 3:
+                System.out.println("3을 선택했습니다.");
+                break;
+
+            default:
+                System.out.println("이상한 것을 선택했습니다.");
+        }
+
+        s.close();
+
+
+    }
+}
