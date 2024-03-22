db359372e9574411806ae252b0653f9ca928bda9
ggoingji95 <river1825@naver.com>
2024-03-22 15:16:56 +0900

    code05_03

---
+++ src/main/java/code05_02.java
@@ -0,0 +1,11 @@
+public class code05_02 {
+    public static void main(String[] args){
+
+        int num = 99;
+
+        if (num < 100);
+
+        System.out.println("100보다 ");
+        System.out.println("작습니다. ");
+    }
+}
---
+++ src/main/java/code05_03.java
@@ -0,0 +1,14 @@
+public class code05_03 {
+    public static void main(String[] args) {
+
+        int num = 200;
+
+        if (num < 100)
+            System.out.println("100보다 작군요.");
+
+        else {
+
+            System.out.println("100보다 크군요.");
+        }
+    }
+}
