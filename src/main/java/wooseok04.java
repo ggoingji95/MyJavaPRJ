5bda3190902723b2cdf656698b13c3d76ca7f7c3
ggoingji95 <river1825@naver.com>
2024-03-24 10:35:08 +0900

    wooseok05

---
+++ src/main/java/wooseok04.java
@@ -0,0 +1,13 @@
+public class wooseok04 {
+    public static void main (String[]args){
+
+        String str1 = "난생처음";
+        String str2 = "자바";
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String result    = str1 + str2;
+        System.out.println(result);
+
+    }
+}
---
+++ src/main/java/wooseok05.java
@@ -0,0 +1,14 @@
+public class wooseok05 {
+    public static void main(String[]args){
+
+        String str1 ="우석생일";
+        String str2 = "4월10일";
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String result    = str1 + str2;
+        System.out.println(str1 +str2);
+
+    }
+}
