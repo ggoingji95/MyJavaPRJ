af753c65b746b5882b30761cbd90ce1b505ccb41
ggoingji95 <river1825@naver.com>
2024-03-24 10:17:41 +0900

    wooseok_01

---
+++ src/main/java/wooseok_01.java
@@ -0,0 +1,13 @@
+public class wooseok_01 {
+    public static void main(String[]args){
+        System.out.println("우석이는 최고야!");
+        System.out.println("우석이는 귀여워!");
+        System.out.println("인정하십니까?");
+
+        int a = 1300;
+        int b = 22250;
+
+        int c = a + b;
+        System.out.println(a + " + "  + b + " = " + c);
+    }
+}
