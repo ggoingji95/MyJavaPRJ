3d7da91a02dc01a01b3d5624d9ceac5d34c4a47c
ggoingji95 <river1825@naver.com>
2024-03-24 10:28:47 +0900

    wooseok03

---
+++ src/main/java/wooseok03.java
@@ -0,0 +1,22 @@
+public class wooseok03 {
+    public static void main(String[]args){
+        int num1, num2, result;
+
+        num1 = 330;
+        num2 = 23;
+
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result);
+
+        result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result);
+
+        result = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + result);
+
+        result = num1 / num2;
+        System.out.println(num1 + " / " + num2 + " = " + result);
+
+
+    }
+}
---
+++ src/main/java/wooseok_02.java
@@ -0,0 +1,12 @@
+public class wooseok_02 {
+    public static void main(String[]args){
+
+        int num1, num2, result;
+
+        num1 = 3500;
+        num2 = 1250;
+
+        result = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + result);
+    }
+}
