14e2df0c301099925ce19cdcee0ec0c48a84622a
ggoingji95 <river1825@naver.com>
2024-03-25 14:00:07 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/code04_10.java
@@ -0,0 +1,13 @@
+public class code04_10 {
+    public static void main(String[]args){
+        String str = "난생처음 자바";
+
+        int len;
+
+        len = str.length();
+
+        System.out.println("문자열 : " + str);
+        System.out.println("문자열 길이 :   " + len);
+
+    }
+}
