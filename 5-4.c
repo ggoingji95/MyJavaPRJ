8db0188184962f913551b42b511fd589e2a321fb
ggoingji95 <162660141+ggoingji95@users.noreply.github.com>
2024-04-15 19:50:09 +0900

    이번년도는 윤년일까?
    사용자 입력값으로 윤년이 맞는지 알아보기

---
+++ 5-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int year;
+	
+	printf("년도를 입력해주세요 : ");
+	scanf("%d",&year);
+
+	if ((year%4==0 && ! 100 ==0) || (year%400==0)) {
+		printf("%d년은 윤년이 맞습니다....",year);}
+	else{
+		printf("%d년은 윤년이 아닙니다....",year);}
+}
