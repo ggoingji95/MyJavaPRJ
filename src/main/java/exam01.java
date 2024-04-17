68b7b06941d9633e493c0480101bb4c5d32a8665
ggoingji95 <river1825@naver.com>
2024-04-17 20:41:53 +0900

    자바를 이용한 구구단

---
+++ src/main/java/exam01.java
@@ -0,0 +1,17 @@
+
+
+public class exam01 {
+    public static void main(String[]args){
+
+        int i,k;
+
+        for(i=2; i<=9; i++){
+            for(k=2; k<=9; k++){
+                System.out.println(i+" * " + k + " = " +(i*k));
+            }
+        }
+
+
+
+    }
+}
