c4167035a9047cd8daf80d1df034f34b53dc574e
ggoingji95 <river1825@naver.com>
2024-04-21 10:42:03 +0900

    1 3 5  7층 피라미드

---
+++ src/main/java/exam11.java
@@ -0,0 +1,17 @@
+public class exam11 {
+    public static void main(String[]args){
+        int i,k;
+        for(i=0; i<4; i++){
+            for(k=1; k<8; k++){
+                if(k>3-i&&k<5+i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
