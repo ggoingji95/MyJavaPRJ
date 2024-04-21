5bcb2b4c3baa9688f6e2841bf902f19fd02638cd
ggoingji95 <river1825@naver.com>
2024-04-21 10:42:03 +0900

    리뷰 테스트

---
+++ src/main/java/S240421_1.java
@@ -0,0 +1,11 @@
+public class S240421_1 {
+    public static void main(String[]args){
+        int a, b, c;
+
+        a = 150;
+        b = 300;
+        c = a +b;
+        System.out.println(c);
+        System.out.println(a+"+"+b+"="+c);
+    }
+}
