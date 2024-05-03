6ca29ecffbf5ad661abff98bd5a8b72bbc3fde87
ggoingji95 <162660141+ggoingji95@users.noreply.github.com>
2024-05-03 12:07:33 +0900

    Add files via upload

--- 8-3.c
+++ 8-3.c
@@ -8,11 +8,11 @@ void main()
 
 	for(i=0; i<=3; i++)
 	{
-		printf("%d번째 숫자를 입력하세요 : ", i+1);
-		scanf("%d" , &aa[i] );
+		printf("%d번째 숫자를 입력해주세요 : ", i+1);
+		scanf("%d", &aa[i]);
 	}
 
-	hap = aa[0]+ aa[1]+ aa[2] + aa[3];
+	hap = aa[0] + aa[1] + aa[2] + aa[3];
 
-	printf(" 합계 ===> %d \n", hap);
+	printf("배열의 합계 ===> %d \n", hap);
 }
