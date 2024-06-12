080c4224415488dec285815e2129524b108455ab
ggoingji95 <162660141+ggoingji95@users.noreply.github.com>
2024-06-12 09:32:16 +0900

    C언어 13-5.c
    구조체와 배열

---
+++ 13-5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+	struct student {
+		char name[10];
+		int kor;
+		int eng;
+		float avg;
+	};
+	
+	struct student s[3];
+
+	int i;
+
+	strycpy(s[0].name, "Kim");
+	s[0].kor = 90;
+	s[0].eng = 80;
+	s[0].avg = (s[0].kor + s[0].eng) / 2.0f;
+
+	strycpy(s[1].name, "Lee");
+	s[1].kor = 70;
+	s[1].eng = 60;
+	s[1].avg =  (s[1].kor +s[1].eng) / 2.0f;
+
+	strcpy(s[2].name, "Park");
+	s[2].kor = 50;
+	s[2].eng = 40;
+	s[2].avg = (s[2].kor + s[2].eng) / 2.0f
+
+		printf(" --구조체 배열 -- \n");
+	for(i=0; i<3; i++)
+	{
+		printf("학생이름 ==> %s\n", s[i].name);
+		printf("국어점수 ==> %d\n", s[i].kor);
+		printf("영어점수 ==> %d\n", s[i].eng);
+		printf("평균점수 ==>%5.1f\n", s[i].avg);
+		printf("\n");
+	}
+
